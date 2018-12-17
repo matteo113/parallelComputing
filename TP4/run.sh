8a84825e40901b79473cc0cac05b4805217fd6a6
BESANCON Matteo#855319 <besanco4@login1.cluster>
2018-12-17 16:26:56 +0100

    Add results v2

--- TP4/run.sh
+++ TP4/run.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 #SBATCH --job-name=laplace
-#SBATCH --output=laplace_whole_algo2000x2000x3000x120.o%j
-#SBATCH --ntasks=120
+#SBATCH --output=laplace_whole_algo3000x3000x3000x1_V2.o%j
+#SBATCH --ntasks=1
 #SBATCH --partition=shared,cui
-#SBATCH --time=00:30:00
+#SBATCH --time=02:00:00
 #SBATCH --constraint=E5-2630V4
 
 echo $SLURM_NODELIST
 
-srun ./laplace 2000 2000 3000
+srun ./laplace 3000 3000 3000
